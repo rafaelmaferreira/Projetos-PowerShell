9a117a1e62721e93ec075545859b065d63834d54
Rafael Martin Alves Ferreira <117859338+rafaferreira011@users.noreply.github.com>
2023-05-09 17:13:02 -0300

    Update Start-Stop.ps1

--- Start-Stop.ps1
+++ Start-Stop.ps1
@@ -1,34 +1,49 @@
 param(
-    [Parameter(Mandatory=$true)]
-    [ValidateNotNullOrEmpty()]
-    [string]$horario,
-    [Parameter(Mandatory=$true)]
-    [ValidateSet('Start','Stop')]
-    [string]$action
- )
+    [Parameter(Mandatory=$true)]
+    [ValidateNotNullOrEmpty()]
+    [string]$horario,
+
+    [Parameter(Mandatory=$true)]
+    [ValidateSet('Start','Stop')]
+    [string]$action
+ )
+
 #<#
 #Authenticate with Azure Automation Run As account (service principal)
 
 Connect-AzAccount -Identity
-#>
-#Script to run
 
-$subs = Get-AzSubscription
+#>
+#
+#Script to run
+$subs = Get-AzSubscription 
 
 Foreach ($sub in $subs) {
-    Set-AzContext $sub
 
-    if($action -eq "Start"){
+    $context = Set-AzContext $sub
 
-        ## Start the VM
-        Get-AzVM | Where-Object {$_.Tags["start"] -like $horario} | Start-AzVM       
+    if($action -eq "Start"){
 
-    }elseif($action -eq "Stop"){
+        ## Start the VM
+        $vms = Get-AzVM | Where-Object {$_.Tags["ambevtech.start"] -like $horario}
+        Foreach ($vm in $vms) {
+            Write-Output "Ligando VM: $($vm.Name)"
+            $status = Start-AzVM -ResourceGroupName $vm.ResourceGroupName -Name $vm.Name
+            $status.Status
+        }
+        
+    }elseif($action -eq "Stop"){
+        ## Stop the VM
 
-        ## Stop the VM
-        Get-AzVM | Where-Object {$_.Tags["stop"] -like $horario} | Stop-AzVM -Force       
-
-    }else{
-        Write-Output "Ação incorreta!"
-    }
+        $vms = Get-AzVM | Where-Object {$_.Tags["ambevtech.stop"] -like $horario}
+        Foreach ($vm in $vms) {
+            Write-Output "Desligando VM: $($vm.Name)"
+            $status = Stop-AzVM -ResourceGroupName $vm.ResourceGroupName -Name $vm.Name -Force
+            $status.Status
+        }
+        
+        
+    }else{
+        Write-Output "Ação incorreta!"
+    }
 }
